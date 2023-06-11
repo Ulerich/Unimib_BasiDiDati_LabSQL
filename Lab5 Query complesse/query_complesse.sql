ac8fb4b9ddcb43d6fc4fc2ddc9b98588f743e8c2
e.ronchetti@campus.unimib.it <e.ronchetti@campus.unimib.it>
2023-06-11 15:42:35 +0200

    Query complesse - Fino alle numero 16

--- Lab5 Query complesse/query_complesse.sql
+++ Lab5 Query complesse/query_complesse.sql
@@ -7,7 +7,7 @@ FROM stipendio AS s JOIN personale_docente AS d ON s.classe = d.Classe_stipendio
 WHERE d.Ruolo = 'Ordinario';
 
 -- Query 2
-SELECT C.Nome, C.Ore_lezione + C.Ore_esercitazione AS Ore_Totali
+SELECT C.Nome, (C.Ore_lezione + C.Ore_esercitazione) AS Ore_Totali
 FROM corso AS C JOIN corso_di_laurea_corsi AS CDLC ON C.Codice_C = CDLC.Codice_c
 	JOIN corso_di_laurea AS CDL ON CDLC.idCorso_di_Laurea = CDL.idCorso_di_Laurea
 WHERE CDL.Tipologia = 'Triennale';
@@ -27,14 +27,117 @@ SELECT DISTINCT C.nome, C.crediti_lezione + C.crediti_esercitazione AS Crediti
 FROM esame AS E JOIN corso AS C ON E.Codice_corso = C.Codice_c
 	JOIN corso_di_laurea_corsi AS CDLC ON C.Codice_c = CDLC.Codice_c
 	JOIN corso_di_laurea AS CDL ON CDLC.idCorso_di_Laurea = CDL.idCorso_di_Laurea
-WHERE E.Codice_corso IS NOT NULL;
+WHERE E.Codice_corso IS NOT NULL AND CDL.Tipologia = 'Triennale';
 
 -- Query 6
-SELECT S.Nome, S.Cognome, S.Matricola_st, citta.Nome
+SELECT S.Nome, 
+	   S.Cognome, 
+       S.Matricola_st, 
+       citta.Nome
 FROM corso AS C JOIN piano_di_studio AS P ON C.Codice_c = P.Codice_corso
-	JOIN studente AS S ON P.Matricola_st = S.Matricola_st
-    JOIN citta ON S.Citta_residenza = citta.idCitta
+JOIN studente AS S ON P.Matricola_st = S.Matricola_st
+JOIN citta ON S.Citta_residenza = citta.idCitta
 WHERE C.Nome = 'Analisi Matematica';
 
--- Query 7
--- Controllare soluzione prof
+-- Query 7 Da Completare
+SELECT D.Nome, 
+	   D.Cognome,
+       D.Matricola_d,
+       citta.Nome
+FROM (corso_di_laurea AS CDL JOIN corso_di_laurea_corsi AS CDLC 
+	ON CDL.idCorso_di_Laurea = CDLC.idCorso_di_Laurea
+	JOIN corso AS C ON CDLC.Codice_c = C.Codice_c
+	JOIN Personale_docente AS D ON C.Docente = D.Matricola_d
+	JOIN stipendio ON D.Classe_stipendio = stipendio.Classe)
+	JOIN citta ON D.Citta_di_residenza = citta.idCitta
+WHERE CDL.Tipologia = 'Triennale' AND stipendio.Valore >= 40000 
+	AND (D.ruolo = 'Ordinario' OR D.ruolo = 'Associato');
+
+-- Query 8
+SELECT S.Nome, 
+	   S.Cognome, 
+       S.Matricola_st, 
+       citta.Nome
+FROM corso_di_laurea AS CDL JOIN corso_di_laurea_corsi AS CDLC 
+	ON CDL.idCorso_di_Laurea = CDLC.idCorso_di_Laurea
+    JOIN corso AS C ON CDLC.Codice_c = C.Codice_c
+    JOIN esame AS e ON C.Codice_c = e.Codice_corso
+	JOIN studente AS S ON e.Matricola_studente = S.Matricola_st
+	JOIN citta ON S.Citta_residenza = citta.idCitta
+WHERE C.Nome = 'Analisi Matematica' AND citta.Regione = 'PIEMONTE' 
+	AND CDL.Tipologia = 'Triennale';
+
+-- Query 9
+-- Stilare una classifica degli studenti (con cognome, nome, matricola e voto) che hanno sostenuto
+-- l'esame con codice E3101Q117 con un voto maggiore di 23, ordinandola per voto decrescente
+
+SELECT s.Cognome,
+	   s.Nome,
+       s.Matricola_st,
+       e.Voto
+FROM studente AS s JOIN esame AS e ON s.Matricola_st = e.Matricola_studente
+	JOIN corso AS c ON e.Codice_corso = c.Codice_c
+WHERE c.Codice_c = 'E3101Q117' AND e.voto > 23
+ORDER BY e.Voto DESC;
+
+-- Query 10
+SELECT COUNT(*) AS StudentiVotoSup23
+FROM studente AS s JOIN esame AS e ON s.Matricola_st = e.Matricola_studente
+	JOIN corso AS c ON e.Codice_corso = c.Codice_c
+WHERE c.Codice_c = 'E3101Q117' AND e.voto > 23;
+
+-- Query 11
+SELECT s.Matricola_st, s.nome, s.cognome,
+	   AVG(e.voto) AS MediaEsami
+FROM studente AS s JOIN esame AS e ON s.Matricola_st = e.Matricola_studente
+	JOIN corso AS c ON e.Codice_corso = c.Codice_c
+GROUP BY s.Matricola_st, s.nome, s.cognome
+ORDER BY MediaEsami DESC;
+
+-- Query 12
+SELECT s.Matricola_st, s.nome, s.cognome, COUNT(e.Codice_e) AS EsamiSvolti
+FROM studente AS s JOIN esame AS e ON s.Matricola_st = e.Matricola_studente
+WHERE s.nome LIKE 'B%'
+GROUP BY s.Matricola_st;
+
+-- Query 13
+(SELECT s.nome, s.cognome, s.Matricola_st
+FROM studente AS s JOIN esame AS e ON s.Matricola_st = e.Matricola_studente
+	JOIN corso AS c ON e.Codice_corso = c.Codice_c
+WHERE c.Codice_c = 'E3101Q020')
+UNION
+(SELECT s1.nome, s1.cognome, s1.Matricola_st
+FROM studente AS s1 JOIN esame AS e1 ON s1.Matricola_st = e1.Matricola_studente
+	JOIN corso AS c1 ON e1.Codice_corso = c1.Codice_c
+WHERE c1.Codice_c = 'E31010Q117');
+-- Ricordati di rinominare le tabelle del secondo argomento diversamente
+
+-- Query 14
+SELECT s.Cognome, s.Nome, s.Matricola_st
+FROM Studente AS s
+JOIN Esame AS e ON e.Matricola_studente = s.Matricola_st
+JOIN Esame AS e1 ON e1.Matricola_studente = s.Matricola_st
+WHERE e.Codice_corso = "E3101Q020" AND e1.Codice_corso = "E3101Q117";
+-- Prof propone anche altra soluzione con Subquery e JOIN
+
+-- Query 15
+SELECT c.Codice_c, c.Nome	
+FROM corso AS c JOIN piano_di_studio AS p ON c.Codice_c = p.Codice_corso
+	JOIN studente AS s ON p.Matricola_st = s.Matricola_st
+WHERE s.Matricola_st = '1492601' AND c.Codice_c NOT IN (
+		SELECT e1.Codice_corso
+        FROM esame AS e1
+        WHERE e1.Matricola_studente = '1492601'
+        );
+
+-- Query 16
+SELECT c.Codice_c, c.Nome	
+FROM corso AS c JOIN piano_di_studio AS p ON c.Codice_c = p.Codice_corso
+	JOIN studente AS s ON p.Matricola_st = s.Matricola_st
+    JOIN corso_di_laurea_corsi AS cdlc ON c.Codice_c = cdlc.Codice_c
+    JOIN corso_di_laurea AS cl ON cdlc.idCorso_di_Laurea = cl.idCorso_di_Laurea
+WHERE cl.tipologia = 'Triennale' AND c.Codice_c NOT IN (
+		SELECT e1.Codice_corso
+        FROM esame AS e1
+        ); -- Non funzionante
+
